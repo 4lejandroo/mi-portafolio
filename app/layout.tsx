3f6046216879af52b3ca5f21a5e08778a0fb0fe6
4lejandroo <rojas.alejandro.1g@gmail.com>
2026-03-04 23:40:55 -0300

    cambio en un nombre de scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-mooth">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
